c98758a4fff504360b70763656cf3510675ed23b
Madda <nobody.youknow@local>
2018-09-18 12:33:15 +0100

    User runner for all examples

--- 3/blue_danube_clumsy.py
+++ 3/blue_danube_clumsy.py
@@ -1,28 +1,3 @@
-#! /usr/bin/env python
-# The line above tells some systems (e.g. Linux/Apple shells) what program to
-# use to execute this script.
-
-##############################################################################
-# You don't need to understand most of this yet- you can just skip to the    #
-# large comment section below if this is all a bit daunting!                 #
-##############################################################################
-
-# Import the libraries we need
-import sys
-from music21 import (
-    environment,
-    metadata,
-    note,
-    stream,
-    )
-
-# Tell music21 what to use to play midi and display score
-environment.set('midiPath', '/usr/bin/timidity')
-environment.set('musicxmlPath', '/usr/bin/musescore')
-
-##############################################################################
-#                            LESSON 3 STARTS HERE                            #
-#############################################################################
 right_hand = stream.Part()
 left_hand = stream.Part()
 
@@ -662,43 +637,7 @@ tune.insert(0, right_hand)
 tune.insert(0, left_hand)
 
 # Add a title
-tune.metadata = metadata.Metadata(title='Python TTTGLS: Lesson 3 part 1')
-
-##########################################################################
-#                        LESSON 3 (PART 1)ENDS HERE                              #
-##########################################################################
-
-# Only run this if the script is executed directly, not imported
-if __name__ == '__main__':
-    # Complain if there were no arguments passed by the user
-    if len(sys.argv) == 1:
-        # First, print a helpful message
-        print('add a "score" argument to see the score.')
-        print('add a "text" argument to see the python objects.')
-        print('add a "midi" argument to hear it.')
-        print('e.g. To hear the tune: {command} midi'.format(
-            command=sys.argv[0],
-            ))
-        # Now exit without doing anything
-        sys.exit()
-
-    # See if the user put the word 'midi' in the arguments
-    if 'midi' in sys.argv:
-        # The stream.Score (tune) object knows how to play itself using the
-        # environment set midi player and will do so when its show method is
-        # called with a 'midi' argument.
-        tune.show('midi')
-
-    # See if the user put the word 'text' in the arguments
-    if 'text' in sys.argv:
-        # The stream.Score (tune) object knows how to display itself as python
-        # objects in text, and will do so when its show method is called with
-        # a 'text' argument.
-        tune.show('text')
-
-    # See if the user put the word 'score' in the arguments
-    if 'score' in sys.argv:
-        # The stream.Score (tune) object knows how to display itself as
-        # musical score, and will do so by default when its show method is
-        # called with no arguments.
-        tune.show()
+tune.metadata = metadata.Metadata(
+    title='Python TTTGLS: Lesson 3 part 1',
+    composer='New programmer',
+)
